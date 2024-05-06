02d0f1285436af2bbd54de14818ef02a0b323747
Ruzal <ruzal02@inbox.ru>
2024-05-06 17:59:35 +0300

    06.05.2024 17:57

--- dist/validations/schemas/dictionary.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createDictionarySchema = void 0;
-const zod_1 = require("zod");
-exports.createDictionarySchema = zod_1.z.object({
-    userId: zod_1.z.string().min(1),
-    languageId: zod_1.z.string().min(1),
-    name: zod_1.z.string().min(1),
-});
